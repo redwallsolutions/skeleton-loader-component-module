8fe3d10da2c59e817dd22e729418aadecd93dcd4
Andrew Ribeiro <andtankian@gmail.com>
2019-07-01 12:01:07 -0300

    First commit created by CRCM.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { render } from "react-dom";
+import { ThemeProvider } from 'styled-components';
+import {ItsWorking} from './lib'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  state = {
+    mode: 'light'
+  }
+
+  changeMode = (input) => {
+    const mode = input.target.id
+    this.setState({
+      mode
+    });
+  }
+
+  render() {
+    return (
+      <ThemeProvider theme={{mode:this.state.mode}}>
+        <div style={{transition: 'all .3s', height: '99vh', width: '98vw', backgroundColor: this.state.mode === 'dark' ? 'rgb(44, 55, 56)' : 'rgb(244, 244, 244)'}}>
+          <div style={{display: 'flex', justifyContent: 'center'}}>
+            <label htmlFor='light'>Light Mode</label>
+            <input type='radio' id='light' name='mode' onChange={this.changeMode}/>
+            <label htmlFor='dark'>Dark Mode</label>
+            <input  type='radio' id='dark' name='mode' onChange={this.changeMode}/>
+          </div>
+          <ItsWorking/>
+        </div>
+      </ThemeProvider>
+    )
+  }
+}
+
+render(<App />, document.getElementById("root"));
